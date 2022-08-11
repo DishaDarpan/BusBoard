6a04f60db3d3b9ad8cc3a57edae9d81a4d13b27c
Noorulain Fahad <Noorulain.Fahad@softwire.com>
2022-08-11 21:27:49 +0100

    Bus Board with Stop Code

--- boarding.js
+++ boarding.js
@@ -1,5 +1,21 @@
-const fetch = require('node-fetch')
-fetch(<https://api.tfl.gov.uk/StopPoint/Mode/bus/Disruption HTTP/1.1
->)
+
+//const fetch = require('node-fetch')
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const readline = require('readline-sync')
+
+// fetch(<https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals>) //490008660N have to replace this with user input StopCode
+//     .then(response => response.json())
+//     .then(body => console.log(body));
+
+
+function getBusStop(){
+
+    console.log('Please enter Bus Stop code: '); 
+    BusStopCode = readline.prompt();
+    const NextBusStops = fetch('https://api.tfl.gov.uk/StopPoint/'+BusStopCode+'/Arrivals') 
     .then(response => response.json())
     .then(body => console.log(body));
+
+}
+
+getBusStop()
